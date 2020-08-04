7bf311b1fce02661dc358f01c4423769ad3e5ef5
soshiharami <soshi0807sss@gmail.com>
2020-08-04 23:38:59 +0900

    :add: testコマンドにメッセージを追加した

--- src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
+++ src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
@@ -2,6 +2,10 @@ package soshiplugin.soshiplugin;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SoshiPlugin extends JavaPlugin {
@@ -22,6 +26,7 @@ public final class SoshiPlugin extends JavaPlugin {
         String cmd_name = cmd.getName();
         if(cmd_name.equalsIgnoreCase("test")){
             getLogger().info("test command");
+            sender.sendMessage("TEST " + "hello " + sender.getName());
         }
         // not found
         return false;
