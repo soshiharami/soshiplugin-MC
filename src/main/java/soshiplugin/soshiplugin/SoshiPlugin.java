dd2b10599e718271c9c5124c92baea185193678d
soshiharami <soshi0807sss@gmail.com>
2020-08-05 00:27:27 +0900

    :add: quickly bed

--- src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
+++ src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
@@ -1,9 +1,12 @@
 package soshiplugin.soshiplugin;
 
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -22,14 +25,22 @@ public final class SoshiPlugin extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-        // test command
         String cmd_name = cmd.getName();
+        // test command
         if(cmd_name.equalsIgnoreCase("test")){
             getLogger().info("test command");
             sender.sendMessage("TEST " + "hello " + sender.getName());
         }
         // not found
         return false;
+    }
+
+    //quickly bed
+    @EventHandler
+    public static void onBed(PlayerBedEnterEvent event){
+        Player p = event.getPlayer();
+        World w = p.getWorld();
+        w.setTime(0);
     }
 
 }
