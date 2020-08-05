896883f1d85f6ed76ad5ecfe1f57df2bd674f3b0
soshiharami <soshi0807sss@gmail.com>
2020-08-05 13:24:26 +0900

    :refactor: split up files.

--- src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
+++ src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
@@ -1,24 +1,26 @@
 package soshiplugin.soshiplugin;
 
 import org.bukkit.*;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
+import soshiplugin.soshiplugin.commands.TestCommand;
+import soshiplugin.soshiplugin.events.Bed;
+import soshiplugin.soshiplugin.events.Login;
 
-public final class SoshiPlugin extends JavaPlugin implements Listener {
+public class SoshiPlugin extends JavaPlugin  {
+
+    private TestCommand CommandManager;
 
     @Override
     public void onEnable() {
         getLogger().info("Plugin activation");
-        getServer().getPluginManager().registerEvents(this, this);
+        getServer().getPluginManager().registerEvents(new Bed(), this);
+        getServer().getPluginManager().registerEvents(new Login(), this);
+        getCommand("test").setExecutor(new TestCommand());
     }
 
     @Override
@@ -26,31 +28,5 @@ public final class SoshiPlugin extends JavaPlugin implements Listener {
         getLogger().info("Plugin shutdown");
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
-        String cmd_name = cmd.getName();
-        // test command
-        if(cmd_name.equalsIgnoreCase("test")){
-            getLogger().info("test command");
-            sender.sendMessage("TEST " + "hello " + sender.getName());
-        }
-        // not found
-        return false;
-    }
 
-    //quickly bed
-    @EventHandler
-    public void onBed(PlayerBedEnterEvent event){
-        getLogger().info("sleeeeeeeeeeeeeeep");
-        Player p = event.getPlayer();
-        World w = p.getWorld();
-        w.setTime(0);
-    }
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer(); // Joinしたプレイヤー
-        player.sendMessage(ChatColor.GOLD + "よく来たな!このとんでもない成金め!!");
-        Location loc = event.getPlayer().getLocation();
-        loc.getWorld().playSound(loc, Sound.UI_TOAST_CHALLENGE_COMPLETE,1f,-0.1f);
-    }
 }
