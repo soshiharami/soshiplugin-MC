0ebc66b8f6119d0fd3a4a50fea2b33c70d9d87ca
soshiharami <soshi0807sss@gmail.com>
2020-08-17 22:32:24 +0900

    :add: command file

--- src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
+++ src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
@@ -3,6 +3,7 @@ package soshiplugin.soshiplugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import soshiplugin.soshiplugin.commands.S_tpCommand;
 import soshiplugin.soshiplugin.commands.TestCommand;
+import soshiplugin.soshiplugin.commands.xptrade;
 import soshiplugin.soshiplugin.events.Bed;
 import soshiplugin.soshiplugin.events.GetExp;
 import soshiplugin.soshiplugin.events.Login;
@@ -17,6 +18,7 @@ public class SoshiPlugin extends JavaPlugin  {
         getServer().getPluginManager().registerEvents(new Login(), this);
         getServer().getPluginManager().registerEvents(new GetExp(), this);
         Objects.requireNonNull(getCommand("test")).setExecutor(new TestCommand());
+        Objects.requireNonNull(getCommand("xptrade")).setExecutor(new xptrade());
         Objects.requireNonNull(getCommand("stp")).setExecutor(new S_tpCommand());
     }
 
--- src/main/java/soshiplugin/soshiplugin/commands/xptrade.java
+++ src/main/java/soshiplugin/soshiplugin/commands/xptrade.java
@@ -1,4 +1,18 @@
 package soshiplugin.soshiplugin.commands;
 
-public class xptrade {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class xptrade implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        String cmd_name = cmd.getName();
+        // test command
+        if(cmd_name.equalsIgnoreCase("xptrade")){
+        }
+        // not found
+        return false;
+    }
 }
