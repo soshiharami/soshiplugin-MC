59f7b883988f9d7c6f8fcd673205671e484ad1ab
soshiharami <soshi0807sss@gmail.com>
2020-08-20 19:06:00 +0900

    :add: open Inventory

--- src/main/java/soshiplugin/soshiplugin/commands/xptrade.java
+++ src/main/java/soshiplugin/soshiplugin/commands/xptrade.java
@@ -1,16 +1,25 @@
 package soshiplugin.soshiplugin.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+import static org.bukkit.Bukkit.getPlayer;
 
 public class xptrade implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
         String cmd_name = cmd.getName();
+        Player player = getPlayer(sender.getName());
         // test command
         if(cmd_name.equalsIgnoreCase("xptrade")){
+            Inventory trade_item_inv = Bukkit.createInventory(null, 9, "trade item inv");
+            assert player != null;
+            player.openInventory(trade_item_inv);
         }
         // not found
         return false;
