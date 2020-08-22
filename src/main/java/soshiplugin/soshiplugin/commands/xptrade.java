ca75bced9fbbeb11661b331f6fce04dce00f1993
soshiharami <soshi0807sss@gmail.com>
2020-08-23 01:28:21 +0900

    :add: Click event

--- src/main/java/soshiplugin/soshiplugin/commands/xptrade.java
+++ src/main/java/soshiplugin/soshiplugin/commands/xptrade.java
@@ -1,8 +1,7 @@
 package soshiplugin.soshiplugin.commands;
 
-import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -12,12 +11,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
+import java.util.Objects;
+
+import static org.bukkit.Bukkit.broadcastMessage;
 import static org.bukkit.Bukkit.getPlayer;
 
 public class xptrade implements CommandExecutor, Listener {
 
-    private static Inventory trade_item_inv_open = null;
+    private static Inventory trade_item_inv_open = Bukkit.createInventory(null, 9, "trade item inv");
     private static Player trade_partner = null;
     private static Player trade_player = null;
     private static float trade_xp = 0;
@@ -33,8 +36,8 @@ public class xptrade implements CommandExecutor, Listener {
         trade_player = player;
         trade_partner = getPlayer(args[0]);
         trade_xp = Float.parseFloat(args[1]);
-        if (trade_partner.getExp() <= trade_xp){
-            assert player != null;
+        Objects.requireNonNull(player).sendMessage(String.valueOf(trade_player.getExp()));
+        if (trade_partner.getLevel() + trade_partner.getExp() <= trade_xp){
             player.sendMessage("This player don't have " + args[1] + " xps");
             return false;
         }
@@ -42,7 +45,6 @@ public class xptrade implements CommandExecutor, Listener {
         if(cmd_name.equalsIgnoreCase("xptrade")){
             Inventory trade_item_inv = Bukkit.createInventory(null, 9, "trade item inv");
             trade_item_inv_open = trade_item_inv;
-            assert player != null;
             player.openInventory(trade_item_inv);
         }
         // not found
@@ -53,15 +55,15 @@ public class xptrade implements CommandExecutor, Listener {
     public void close_inv(InventoryCloseEvent event){
         if(trade_item_inv_open != event.getInventory()) return;
         trade_partner.sendMessage(trade_player.getName() + "から" + trade_xp + "のトレードが来ています");
-        sendClickText(trade_player,"受けるならここをクリック","/xptrade open");
-    }
-
-    public static void sendClickText(Player p,String text,String command){
-        ClickEvent clickEvent = null;
-        if(command != null){
-            clickEvent = new ClickEvent(ClickEvent.Action.RUN_COMMAND,command);
+        StringBuilder Item = new StringBuilder();
+        for (ItemStack item: trade_item_inv_open.getContents()){
+            if (item == null) continue;
+            Item.append(item.getItemMeta().getDisplayName()).append("  × ").append(item.getAmount()).append("\n");
+            //APIのバグで名前が出ません
         }
-        BaseComponent[] message = new ComponentBuilder(text).event(clickEvent). create();
-        p.spigot().sendMessage(message);
+        trade_player.sendMessage(Item.toString());
+        TextComponent message = new TextComponent( "Click me" );
+        message.setClickEvent( new ClickEvent( ClickEvent.Action.RUN_COMMAND, "/xptrade open 0" ) );
+        trade_player.spigot().sendMessage(message);
     }
 }
