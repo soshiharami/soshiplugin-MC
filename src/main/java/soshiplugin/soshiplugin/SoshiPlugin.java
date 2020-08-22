c79a2b46e04e40328ffa17d7540154a988dfb688
soshiharami <soshi0807sss@gmail.com>
2020-08-22 19:35:36 +0900

    :add: Inventory close event

--- src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
+++ src/main/java/soshiplugin/soshiplugin/SoshiPlugin.java
@@ -17,6 +17,7 @@ public class SoshiPlugin extends JavaPlugin  {
         getServer().getPluginManager().registerEvents(new Bed(), this);
         getServer().getPluginManager().registerEvents(new Login(), this);
         getServer().getPluginManager().registerEvents(new GetExp(), this);
+        getServer().getPluginManager().registerEvents(new xptrade(), this);
         Objects.requireNonNull(getCommand("test")).setExecutor(new TestCommand());
         Objects.requireNonNull(getCommand("xptrade")).setExecutor(new xptrade());
         Objects.requireNonNull(getCommand("stp")).setExecutor(new S_tpCommand());
--- src/main/java/soshiplugin/soshiplugin/commands/xptrade.java
+++ src/main/java/soshiplugin/soshiplugin/commands/xptrade.java
@@ -5,13 +5,18 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.Inventory;
 
 import java.util.Objects;
 
 import static org.bukkit.Bukkit.getPlayer;
 
-public class xptrade implements CommandExecutor {
+public class xptrade implements CommandExecutor, Listener {
+
+    public static Inventory trade_item_inv_open = null;
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
@@ -20,11 +25,18 @@ public class xptrade implements CommandExecutor {
         // test command
         if(cmd_name.equalsIgnoreCase("xptrade")){
             Inventory trade_item_inv = Bukkit.createInventory(null, 9, "trade item inv");
+            trade_item_inv_open = trade_item_inv;
             assert player != null;
             player.openInventory(trade_item_inv);
-            player.sendMessage(Objects.requireNonNull(Objects.requireNonNull(trade_item_inv.getItem(0)).getItemMeta()).getDisplayName());
         }
         // not found
         return false;
+    }
+
+    @EventHandler
+    public void close_inv(InventoryCloseEvent event){
+        if(trade_item_inv_open != event.getInventory()) return;
+        String item = Objects.requireNonNull(event.getInventory().getItem(0)).toString();
+        event.getPlayer().sendMessage(item);
     }
 }
