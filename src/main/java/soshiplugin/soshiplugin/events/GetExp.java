416e511bc549ab77ae271c3a9a1d85b0cfb1bc9c
soshiharami <soshi0807sss@gmail.com>
2020-09-18 14:27:18 +0900

    :refactor:

--- src/main/java/soshiplugin/soshiplugin/events/GetExp.java
+++ src/main/java/soshiplugin/soshiplugin/events/GetExp.java
@@ -7,11 +7,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.world.StructureGrowEvent;
 
+import java.util.Objects;
+
 
 public class GetExp implements Listener {
 
     @EventHandler
-    public void Break_Block(BlockBreakEvent event){
+    public void Break_Block(BlockBreakEvent event) {
         Player player = event.getPlayer();
         if (event.isDropItems()) { //アイテムをドロップしたら
             player.giveExp(1);
@@ -19,9 +21,9 @@ public class GetExp implements Listener {
     }
 
     @EventHandler
-    public void StructureGrow(StructureGrowEvent event){
+    public void StructureGrow(StructureGrowEvent event) {
         Player player = event.getPlayer();
-        player.giveExp(1);
+        Objects.requireNonNull(player).giveExp(1);
     }
 
 }
