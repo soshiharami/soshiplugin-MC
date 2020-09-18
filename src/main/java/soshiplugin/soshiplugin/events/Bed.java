a6718620e51ca9f2f51c6bfc0386e474f66b2167
soshiharami <soshi0807sss@gmail.com>
2020-09-18 14:26:43 +0900

    :refactor:

--- src/main/java/soshiplugin/soshiplugin/events/Bed.java
+++ src/main/java/soshiplugin/soshiplugin/events/Bed.java
@@ -5,13 +5,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
-import static org.bukkit.Bukkit.getLogger;
 
 
 public class Bed implements Listener {
     //quickly bed
     @EventHandler
-    public void onBed(PlayerBedEnterEvent event){
+    public void onBed(PlayerBedEnterEvent event) {
         Player p = event.getPlayer();
         World w = p.getWorld();
         w.setTime(0);
